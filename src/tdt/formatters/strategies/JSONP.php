e5e6819459f4e498f6569f073d32fbbd414508b4
Jan Vansteenlandt <vansteenlandt.jan@gmail.com>
2013-10-25 14:18:24 +0200

    Update JSONP.php

--- src/tdt/formatters/strategies/JSONP.php
+++ src/tdt/formatters/strategies/JSONP.php
@@ -9,34 +9,36 @@
  */
 
 namespace tdt\formatters\strategies;
+use tdt\exceptions\TDTException;
 
-class JSONP extends \tdt\formatters\AStrategy{
+class JSONP extends \tdt\formatters\strategies\JSON{
 
-     private $callback;
+	private $callback;
 
-     public function __construct($rootname,$objectToPrint,$callback = ""){
-	  if($callback != ""){
-	       $this->callback = $callback;
-	  }else{
-	       throw new \tdt\framework\TDTException(452, array("With Jsonp you should add a callback: &callback=yourfunctionname"));
-	  }
-	  parent::__construct($rootname,$objectToPrint);
-     }
+	public function __construct($rootname,$objectToPrint,$callback = ""){
 
-     public function printHeader(){
-	  header("Access-Control-Allow-Origin: *");
-	  header("Content-Type: application/json;charset=UTF-8");	  
-     }
+		parent::__construct($rootname,$objectToPrint);
+		if(empty($_GET['callback'])){
+			echo "You must pass along a request parameter called callback in order to use the JSONP functionality.";
+			exit();
+		}
 
-     public function printBody(){
-	  echo $this->callback . '(';
-	  parent::printBody();
-	  echo ')';
-     }
+	}
+
+	public function printHeader(){
+		header("Access-Control-Allow-Origin: *");
+		header("Content-Type: application/javascript;charset=UTF-8");
+	}
+
+	public function printBody(){
+		echo  $_GET['callback'] . '(';
+       	parent::printBody();
+        $json = ob_get_contents();
+		echo ')';
+	}
 
 
-    public static function getDocumentation(){
-        return "Prints json but will wrap the output in the callback function specified";
-    }
-};
-?>
+public static function getDocumentation(){
+	return "Prints json but will wrap the output in the callback function specified";
+}
+}
